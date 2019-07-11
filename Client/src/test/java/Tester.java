e1c35fa29d8f7141fe485e5e942f95192c14dc08
Nils <nils.gamlin@gmail.com>
2019-07-11 13:16:00 +0200

    Cleaned

--- Client/src/test/java/Tester.java
+++ Client/src/test/java/Tester.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
